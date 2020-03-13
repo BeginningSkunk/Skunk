1d3ce3f261b71bf682da625636888855422da9c8
farahnorma <farahnorma2@gmail.com>
2020-03-13 18:46:26 -0500

    fourth commit

--- SkunkProject/src/SkunkApp.java
+++ SkunkProject/src/SkunkApp.java
@@ -5,8 +5,12 @@ public class SkunkApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Dice dice1 = new Dice();
+		
+		
 		dice1.roll();
 		StdOut.println("main beginning");
+		
+		StdOut.println(dice1.getLastRoll());
 	}
 
 }
