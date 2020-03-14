4f1673f06dce3e229cb5c52b325f7bcb14d3f546
farahnorma <farahnorma2@gmail.com>
2020-03-13 19:04:38 -0500

    fifth commit- need to add specific tests

---
+++ SkunkProject/src/DiceTest.java
@@ -0,0 +1,29 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DiceTest {
+
+	@Test
+	void test1() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void test2() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void test3() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void test4() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void test5() {
+		fail("Not yet implemented");
+	}
+	
+
+}
---
+++ SkunkProject/src/DieTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DieTest {
+
+	@Test
+	void test1() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void test2() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void test3() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void test4() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void test5() {
+		fail("Not yet implemented");
+	}
+}
