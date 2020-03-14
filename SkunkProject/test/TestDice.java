711d3f9b85703271c819bf426414643a7377c848
farahnorma <farahnorma2@gmail.com>
2020-03-13 20:15:31 -0500

    testdice updated with 6 tests

--- SkunkProject/test/TestDice.java
+++ SkunkProject/test/TestDice.java
@@ -1,3 +1,4 @@
+  
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -14,35 +15,35 @@ class TestDice {
     }
 	@Test
 	void test2() {
-		testableDice dice = new testableDice();
+	testableDice dice = new testableDice();
         testableDie die1 = new testableDie();
         testableDie die2 = new testableDie();
         int total = die1.getLastRoll() + die2.getLastRoll();
-		dice.getLastRoll();
+	dice.getLastRoll();
 		
-		assertEquals(dice.getLastRoll(), total);
+	assertEquals(dice.getLastRoll(), total);
 	}
 	@Test
 	void test3() {
-		testableDice dice = new testableDice();
+	testableDice dice = new testableDice();
         dice.getLastRoll();
         assertNotEquals(dice.getLastRoll(), 13);
 	}
 	@Test
 	void test4() {
-		testableDice dice = new testableDice();
+	testableDice dice = new testableDice();
         dice.getLastRoll();
         assertNotEquals(dice.getLastRoll(), -12);
 	}
 	@Test
 	void test5() {
-		testableDice dice = new testableDice();
+	testableDice dice = new testableDice();
         dice.getLastRoll();
         assertNotEquals(dice.getLastRoll(), 1);
 	}
 	@Test
 	void test6() {
-		testableDice dice = new testableDice();
+	testableDice dice = new testableDice();
         dice.getLastRoll();
         assertNotEquals(dice.getLastRoll(), 6);
 	}
