e764f9e7d1f6e17cfd6b50cb37f0c65d47fdcfa1
farahnorma <farahnorma2@gmail.com>
2020-03-13 20:01:24 -0500

    2 tests

--- SkunkProject/test/TestDice.java
+++ SkunkProject/test/TestDice.java
@@ -8,14 +8,19 @@ class TestDice {
 
 	@Test
 	void test1() {
-        testableDie die = new testableDie();
-        die.getLastRoll();
-        assertEquals(die.getLastRoll(), 6);
-    
-	}
+        testableDice dice = new testableDice();
+        dice.getLastRoll();
+        assertEquals(dice.getLastRoll(), 12);
+    }
 	@Test
 	void test2() {
-		fail("Not yet implemented");
+		testableDice dice = new testableDice();
+        testableDie die1 = new testableDie();
+        testableDie die2 = new testableDie();
+        int total = die1.getLastRoll() + die2.getLastRoll();
+		dice.getLastRoll();
+		
+		assertEquals(dice.getLastRoll(), total);
 	}
 	@Test
 	void test3() {
